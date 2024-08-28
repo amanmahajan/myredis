6fc539cf917388616a2fe5d987ffdcd8c8e0bdc8
Aman Mahajan <amanmahajan@Amans-MacBook-Pro.local>
2024-08-28 09:04:02 -0700

    Evaluate Key Stats

--- main/core/eviction.go
+++ main/core/eviction.go
@@ -6,13 +6,28 @@ func evictKeys() {
 	switch config.EvictionStrategy {
 	case "simple_first":
 		evictFirst()
+	case "allkeys-random":
+		evictAllKeyRandom()
 	}
 
 }
 
 func evictFirst() {
 	for k := range store {
-		delete(store, k)
+		Delete(k)
 		return
+	}
+}
+
+func evictAllKeyRandom() {
+
+	totalKeysToEvict := int64(config.EvictionRatio * float64(config.KeyLimits))
+	// Golang Dictionary has random insertion
+	for k := range store {
+		Delete(k)
+		totalKeysToEvict--
+		if totalKeysToEvict <= 0 {
+			return
+		}
 	}
 }
